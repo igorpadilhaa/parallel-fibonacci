57806d47eb054e13a691356f029fd915cec366d0
igorpadilhaa <igor.padilha2004@gmail.com>
2025-03-06 23:00:22 -0400

    add support for big integers

--- main.go
+++ main.go
@@ -4,61 +4,38 @@ import (
     "fmt"
     "time"
     "runtime"
+    "math/big"
 )
 
-func fib(pos uint64) uint64 {
+func bigI(a int64) *big.Int {
+    return big.NewInt(a);
+}
+
+func sum(a, b *big.Int) *big.Int {
+    var z big.Int;
+    return z.Add(a, b);
+}
+
+func sub(a, b *big.Int) *big.Int {
+    var z big.Int;
+    return z.Sub(a, b);
+}
+
+func mul(a, b *big.Int) *big.Int {
+    var m big.Int;
+    return m.Mul(b, a);
+}
+
+func fib(pos int64) *big.Int {
     if pos <= 1 {
-        return pos;
+        return big.NewInt(pos);
     }
-    return fib(pos - 1) + fib(pos - 2); 
+    return sum(fib(pos - 1), fib(pos - 2));
 }
 
-func fibCached(pos uint64) uint64 {
-    return fibWithCache(pos, make([]uint64, pos));
-}
-
-func fibWithCache(pos uint64, cache []uint64) uint64 {
+func fibJump(pos int64) *big.Int {
     if pos <= 1 {
-        return pos;
-    }
-    
-    if cache[pos - 2] != 0 {
-        return cache[pos - 2];
-    }
-
-    value := fibWithCache(pos - 1, cache) + fibWithCache(pos - 2, cache);
-    if uint64(len(cache)) < pos {
-        return value;
-    }
-
-    cache[pos - 2] = value;
-    return value;
-}
-
-func fibCount(pos uint64) ([]uint64, []uint64) {
-    count := make([]uint64, pos + 1);
-    cache := make([]uint64, pos + 1);
-    
-    fibWithCacheAndCount(pos, cache, count); 
-    return count, cache;
-}
-    
-func fibWithCacheAndCount(pos uint64, cache []uint64, count []uint64) uint64 {
-    if pos <= 1 {
-        count[pos]++;
-        cache[pos] = pos;
-        return pos;
-    }
-
-    value := fibWithCacheAndCount(pos - 1, cache, count) + fibWithCacheAndCount(pos - 2, cache, count);
-    count[pos]++;
-    cache[pos] = value;
-    return value;
-}
-
-func fibJump(pos uint64) uint64 {
-    if pos <= 1 {
-        return pos
+        return bigI(pos);
     }
 
     const psize = 20;
@@ -66,40 +43,40 @@ func fibJump(pos uint64) uint64 {
     i, i0, i1 := fibIndexer(psize);
     f, f0 := i, i0;
 
-    for iter := uint64(1); iter < pos / psize; iter++ {
-        unit := f + f0;
+    for iter := int64(1); iter < pos / psize; iter++ {
+        unit := sum(f, f0);
 
-        f0 = i0 * unit + i1 * f;
-        f  = i  * unit + i0 * f;
+        f0 = sum(mul(i0, unit), mul(i1, f));
+        f  = sum(mul(i, unit),  mul(i0, f));
     }
 
     if remain := pos % psize; remain != 0 {
         i, i0, i1 := fibIndexer(remain); 
-        unit := f + f0;
+        unit := sum(f, f0);
 
-        f0 = i0 * unit + i1 * f;
-        f  = i  * unit + i0 * f;
+        f0 = sum(mul(i0, unit), mul(i1, f));
+        f  = sum(mul(i, unit),  mul(i0, f));
     }
     return f;
 }
 
-func fibIndexer(pos uint64) (k, k0, k1 uint64) {
-    k, k0, k1 = 1, 1, 0;
+func fibIndexer(pos int64) (k, k0, k1 *big.Int) {
+    k, k0, k1 = bigI(1), bigI(1), bigI(0);
 
     if pos == 2 {
-        return 1, 1, 0;
+        return;
     }
 
     if pos == 1 {
-        return 1, 0, 0;
+        return bigI(1), bigI(0), bigI(0);
     }
 
     if pos == 0 {
-        return 0, 0, 0;
+        return bigI(0), bigI(0), bigI(0);
     }
 
     for pos > 2 {
-        tmp := k + k0;
+        tmp := sum(k, k0);
         k1 = k0;
         k0 = k;
         k = tmp;
@@ -108,42 +85,42 @@ func fibIndexer(pos uint64) (k, k0, k1 uint64) {
     return;
 }
 
-type FibResult struct {
-    k, k0 uint64;    
-}
-
-func binFibJump(pos uint64) (uint64, uint64) {
+func binFibJump(pos int64) (*big.Int, *big.Int) {
     if pos == 2 {
-        return 1, 1;
+        return bigI(1), bigI(1);
     }
 
     if pos == 1 {
-        return 1, 0;
+        return bigI(1), bigI(0);
     }
 
     if pos == 0 {
-        return 0, 0;
+        return bigI(0), bigI(0);
     }
 
     middle := pos / 2;
     f, f0 := binFibJump(middle);
 
-    f1 := f - f0;
-    unit := f + f0;
+    f1 := sub(f, f0);
+    unit := sum(f, f0);
     
-    v0 := f0 * unit + f1 * f;
-    v  := f * unit + f * f0;
+    v0 := sum(mul(f0, unit), mul(f1, f));
+    v  := sum(mul(f, unit), mul(f, f0));
     
     if pos % 2 == 0 {
         return v, v0;
     }
-    return v + v0, v;
+    return sum(v, v0), v;
 }
 
-func multiFibJump(pos uint64) uint64 {
+type FibResult struct {
+    k, k0 *big.Int;
+};
+
+func multiFibJump(pos int64) *big.Int {
     parts := runtime.NumCPU();
 
-    if uint64(parts) > pos {
+    if int64(parts) > pos {
         parts = 1;
     }
 
@@ -151,7 +128,7 @@ func multiFibJump(pos uint64) uint64 {
     results := make([]FibResult, parts);
     outputs := make([]chan FibResult, parts);
 
-    jmpSize := pos / uint64(parts);
+    jmpSize := pos / int64(parts);
 
     for i := range outputs {
         outputs[i] = make(chan FibResult, 10);
@@ -163,7 +140,7 @@ func multiFibJump(pos uint64) uint64 {
         close(outputs[i]);
     }
 
-    var f, f0 uint64
+    var f, f0 *big.Int
     f, f0 = results[0].k, results[0].k0
 
     for i := 1; i < len(results); i++ {
@@ -172,41 +149,41 @@ func multiFibJump(pos uint64) uint64 {
         i := r.k
         i0 := r.k0
 
-        i1 := i - i0;
+        i1 := sub(i, i0);
 
-        unit := f + f0;
+        unit := sum(f, f0);
         
-        f0 = i0 * unit + i1 * f;
-        f  = i * unit + i0 * f;
+        f0 = sum(mul(i0, unit), mul(i1, f));
+        f  = sum(mul(i, unit),  mul(i0, f));
     }
 
-    if exd := pos % uint64(parts); exd != 0 {
+    if exd := pos % int64(parts); exd != 0 {
         i, i0, i1 := fibIndexer(exd);
 
-        unit := f + f0;
+        unit := sum(f, f0);
         
-        f0 = i0 * unit + i1 * f;
-        f  = i * unit + i0 * f;
+        f0 = sum(mul(i0, unit), mul(i1, f));
+        f  = sum(mul(i, unit),  mul(i0, f));
     }
     return f;
 }
 
-func fibSeq(cache []uint64) {
+func fibSeq(cache []*big.Int) {
     if len(cache) < 2 {
         return;
     }
 
-    if (cache[0] == 0 || cache[1] == 0) {
-        cache[0] = 0;
-        cache[1] = 1;
+    if cache[0] == nil {
+        cache[0] = bigI(0);
+        cache[1] = bigI(1);
     }
 
     for i := 2; i < len(cache); i++ {
-        cache[i] = cache[i - 1] + cache[i - 2];
+        cache[i] = sum(cache[i - 1], cache[i - 2]);
     }
 }
 
-func mFibSeq(cache []uint64) {
+func mFibSeq(cache []*big.Int) {
     parts := runtime.NumCPU();
 
     if len(cache) < parts {
@@ -223,12 +200,25 @@ func mFibSeq(cache []uint64) {
 
     for i := 0; i < parts; i++ {
         pos := i * interval;
-        f, f0 := binFibJump(uint64(pos));
+        var f, f0 *big.Int 
+
+        if i != 0 {
+            f, f0 = binFibJump(int64(pos));
+            tmp := f0;
+            f0 = f;
+            f = sum(f, tmp);
+
+        } else {
+            f, f0 = bigI(1), bigI(0);
+        }
 
         slice := cache[pos:pos+interval];
-        slice[1] = f + f0;
-        slice[0] = f;
+        slice[1] = f;
+        slice[0] = f0;
 
+        fmt.Printf("Slicing %d, %d\n", pos, pos+interval);
+        fmt.Println("s[0] = ", slice[0]);
+        fmt.Println("s[1] = ", slice[1]);
         go asyncFibSeq(slice, channels[i]);
     }
 
@@ -243,23 +233,23 @@ func mFibSeq(cache []uint64) {
     }
 }
 
-func asyncFibSeq(cache []uint64, ended chan bool) {
+func asyncFibSeq(cache []*big.Int, ended chan bool) {
     fibSeq(cache);
     ended <- true;
 }
 
-func asyncFib(pos uint64, out chan FibResult) {
+func asyncFib(pos int64, out chan FibResult) {
     k, k0 := binFibJump(pos)
     out <- FibResult{k, k0};
 }
 
 func makeTest() {
     titles := []string{ "Fib seq", "Fib multi seq" };
-    fn := []func([]uint64){ fibSeq, mFibSeq };
+    fn := []func([]*big.Int){ fibSeq, mFibSeq };
 
     for i := range titles {
         fmt.Println("---", titles[i]);
-        cache := make([]uint64, 100);
+        cache := make([]*big.Int, 100);
 
         start := time.Now();
         fn[i](cache);
